482748792a6c8e2dfe90d34d16f6490a42750748
chanhyeok95 <44300260+chanhyeok95@users.noreply.github.com>
2019-10-18 15:55:41 +0800

    extra stuff

--- health-in-action/www/assets/js/signup.js
+++ health-in-action/www/assets/js/signup.js
@@ -14,35 +14,36 @@ var app = {
         document.getElementById("signup-button").addEventListener("click", onSignUpClickHandler);
 
         function onSignUpClickHandler() {
-            const username = document.getElementById("username").value
-            const password = document.getElementById("password").value
-            const email = document.getElementById("email").value
+            // const username = document.getElementById("username").value
+            // const password = document.getElementById("password").value
+            // const email = document.getElementById("email").value
 
-            console.log('hi')
+            // console.log('hi')
 
-            const signUpData = {
-                username: username,
-                password: password,
-                email: email
-            }
+            // const signUpData = {
+            //     username: username,
+            //     password: password,
+            //     email: email
+            // }
 
-            Http.open("POST", 'http://127.0.0.1:5000/login');
-            Http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+            // Http.open("POST", 'http://127.0.0.1:5000/login');
+            // Http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 
-            Http.send(JSON.stringify(signUpData));
+            // Http.send(JSON.stringify(signUpData));
 
-            Http.onreadystatechange = (e) => {
-                if (Http.readyState == 4 && Http.status == 200) {
-                    alert("Signup Successful!")
-                    window.localStorage.setItem(username, password)
-                    location.replace('login.html')
-                } else if (Http.readyState == 4 && Http.status == 401) {
-                    alert("Signup Unsuccessful")
-                    // location.replace('loginfail.html')
-                    // alert("Wrong ID or Password! Please check again!")
+            // Http.onreadystatechange = (e) => {
+            //     if (Http.readyState == 4 && Http.status == 200) {
+            //         alert("Signup Successful!")
+            //         window.localStorage.setItem(username, password)
+            //         location.replace('login.html')
+            //     } else if (Http.readyState == 4 && Http.status == 401) {
+            //         alert("Signup Unsuccessful")
+            //         // location.replace('loginfail.html')
+            //         // alert("Wrong ID or Password! Please check again!")
 
-                }
-            }
+            //     }
+            // }
+            location.replace("form.html")
         }
     }
 };
