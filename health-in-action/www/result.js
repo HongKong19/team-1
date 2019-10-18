83e7b8c52894e0486307230c1ca009bafb188222
Ajay Rajnikanth <ajay.rajnikanth@gmail.com>
2019-10-18 14:42:49 +0800

    result call

---
+++ health-in-action/www/result.js
@@ -0,0 +1,18 @@
+
+$("#userdet").submit(function(event){
+    event.preventDefault();
+    $.ajax({
+        url: 'http://localhost:3001/signup',
+        method: 'post',
+        data: {username: $("#name").val(), password:"",weight: $("#weight").val(), height: $("#height").val(), bp: $("#blood_pressure").val(),sugar:$("#bloodsugar").val(),smoke:$("#smoking").val(),alcohol:$("#alcoholic").val(),foodpref:$("#preference").val()},
+        crossDomain: true
+      })
+      .then(function(success){
+        if (success === ""){
+            alert("Success");
+            window.open ('results.html','_self',false);
+            console.log("worked");
+        }
+        });
+});
+
